70eee08d182c1c9b525165b7f114c1f812458bfc
minhquocece <lonelyblue.hb@gmail.com>
2019-04-29 17:46:51 +0700

    add user story

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,5 +1,36 @@
-const user = [];
+const users = [];
 
 // addUser, removeUser, getUser, getUsersInRoom
 
-const addUser = () => {};
+const addUser = ({ id, username, room }) => {
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  if (!username || !room) {
+    return {
+      error: "Username and room are required!"
+    };
+  }
+
+  const existingUser = users.find(user => {
+    return user.room === room && user.username === username;
+  });
+
+  if (existingUser) {
+    return {
+      error: "Username is in use!"
+    };
+  }
+
+  // Store user
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+
+const removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
